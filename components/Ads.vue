877e7101d675768328145a1e55d8540798bc10b9
yannick lescure <yannick.lescure@gmail.com>
2021-07-15 20:43:38 -0400

    improve code + fix bugs

--- components/Ads.vue
+++ components/Ads.vue
@@ -2,7 +2,7 @@
   <div v-if="!currentUser.freemium" class="w-100 my-3">
     <adsbygoogle
       :ad-style="{ display: 'block' }"
-      ad-slot="7782275531"
+      ad-slot="9731748072"
     />
   </div>
 </template>
